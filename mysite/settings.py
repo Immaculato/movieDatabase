998cb71ec2096d832a79a33855849142e73b5c5b
Lucian Hymer <lblakehymer@gmail.com>
2017-04-14 14:15:41 -0400

    got templates working

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-			'/Users/markdanielng/Documents/Python/mysite/templates',
+			'templates',
 		],
         'APP_DIRS': True,
         'OPTIONS': {
