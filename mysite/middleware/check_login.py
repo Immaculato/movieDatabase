d71e7bd36d44c64138a9fa1a91592e546e4e593e
Lucian Hymer <lblakehymer@gmail.com>
2017-04-15 18:33:30 -0400

    Allow register page to be visited without being logged in

--- mysite/middleware/check_login.py
+++ mysite/middleware/check_login.py
@@ -3,7 +3,7 @@ from mysite.views import log_in
 
 def CheckLogin(get_response):
 	def middleware(request):
-		if '/admin/' in request.path or '/login/' in request.path or 'email' in request.session:
+		if '/register/' in request.path or '/admin/' in request.path or '/login/' in request.path or 'email' in request.session:
 			response = get_response(request)
 		else:
 			response = redirect('/login/',request)
