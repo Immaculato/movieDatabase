3a2021179ff1b69ac152da02d7aaf2328a198720
Tristan <tristan.a.basil@gmail.com>
2017-04-20 19:56:50 -0400

    Reverted all recent changes. Previous commits will still live on github and can be checked out using 'git checkout'. It is also possible to fork a checked out commit if desired.

--- mysite/middleware/check_login.py
+++ mysite/middleware/check_login.py
@@ -5,6 +5,12 @@ def CheckLogin(get_response):
 	def middleware(request):
 		if '/register/' in request.path or '/admin/' in request.path or '/login/' in request.path or 'email' in request.session:
 			response = get_response(request)
+		#option to display homepage without login if we want it later
+			"""
+		elif request.path == "/":
+			print(request.path)
+			response = get_response(request)
+			"""
 		else:
 			response = redirect('/login/',request)
 		
